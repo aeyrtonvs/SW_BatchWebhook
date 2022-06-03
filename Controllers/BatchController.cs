6ae87f9a0b976895ec6b04ed5687a57952b9d0d1
Aeyrton Villalobos <aeyrtonvs@gmail.com>
2022-06-03 14:49:53 -0500

    Batch Controller

---
+++ Controllers/BatchController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SW_APIS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SW_APIS.Controllers
+{
+    [Route("/batch/")]
+    public class BatchController : Controller
+    {
+        [Route("webhook")]
+        [Authorize]
+        public async Task Webhook([FromBody]BatchModel batchRequest)
+        {
+
+        }
+    }
+}
---
+++ Models/BatchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SW_APIS.Models
+{
+    public class BatchModel
+    {
+        public string UrlOutput { get; set; }
+        public string UrlReport { get; set; }
+        public string UrlReportError { get; set; }
+    }
+}
