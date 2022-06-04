c1ecb6623cab59a4a2cbe087cfd93c7faba601a5
Aeyrton Villalobos <aeyrtonvs@gmail.com>
2022-06-03 20:37:13 -0500

    Save report

--- Services/BatchService.cs
+++ Services/BatchService.cs
@@ -2,6 +2,7 @@
 using SW_APIS.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -10,36 +11,29 @@ namespace SW_APIS.Services
 {
     public class BatchService
     {
-        public static async void SendMail(List<string> emailTo, string subject)
+        public static async Task<ResponseModel> SaveReport(BatchModel batch)
         {
-            //try
-            //{
-            //    var mensaje = new MimeMessage();
-            //    mensaje.From.Add(new MailboxAddress("Batch Service", ""));
-            //    mensaje.To.Add(new MailboxAddress("", to));
-            //    mensaje.Subject = subject;
+            var status = "success";
+            try
+            {
+                var dir = String.Format("{0}\\Resources\\report\\reports.txt", Directory.GetCurrentDirectory());
+                var content = String.Format("{0}\nOutput     -----> {1}\nReport     -----> {2}\nError      -----> {3}\n\n", DateTime.Now.ToString(), batch.UrlOutput, batch.UrlReport, batch.UrlReportError);
+                File.WriteAllText(dir, content);
+            }
+            catch(Exception e)
+            {
+                status = "error";
+            }
 
-            //    var bodyBuilder = new BodyBuilder();
-            //    bodyBuilder.HtmlBody = content;
-            //    bodyBuilder.Attachments.Add(file);
-            //    mensaje.Body = bodyBuilder.ToMessageBody();
-
-            //    using (var client = new SmtpClient("myHost", myPort))
-            //    {
-            //        client.Send(mensaje);
-            //    }
-
-            //    estado = true;
-            //    return estado;
-            //}
-            //catch (Exception ex)
-            //{
-            //    return estado;
-            //}
+            return new ResponseModel()
+            {
+                status = status,
+                message = String.Empty
+            };
         }
         public static ResponseModel ValidateRequest(BatchModel batchRequest)
         {
-            if(batchRequest is null)
+            if(batchRequest is null || (batchRequest.UrlOutput is null || batchRequest.UrlReport is null || batchRequest.UrlReportError is null))
             {
                 return new ResponseModel()
                 {
@@ -47,10 +41,7 @@ namespace SW_APIS.Services
                     message = "El request viene vacío."
                 };
             }
-            return new ResponseModel()
-            {
-                status = "success"
-            };
+            return new ResponseModel();
         }
     }
 }
