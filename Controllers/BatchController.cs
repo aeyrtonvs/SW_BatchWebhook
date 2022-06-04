d028c94f8e20d62074da4e557dc0ff7acf8d89a0
Aeyrton Villalobos <aeyrtonvs@gmail.com>
2022-06-03 19:04:50 -0500

    Add webhook controller

--- Controllers/BatchController.cs
+++ Controllers/BatchController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SW_APIS.Models;
+using SW_APIS.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,16 @@ namespace SW_APIS.Controllers
     public class BatchController : Controller
     {
         [Route("webhook")]
+        [HttpPost]
         [Authorize]
-        public async Task Webhook([FromBody]BatchModel batchRequest)
+        public async Task<IActionResult> Webhook([FromBody]BatchModel batchRequest)
         {
-
+            var validateRequest = BatchService.ValidateRequest(batchRequest);
+            if (validateRequest.status != "success")
+            {
+                return BadRequest(validateRequest);
+            }
+            return Ok(validateRequest);
         }
     }
 }
--- Services/BatchService.cs
+++ Services/BatchService.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using MimeKit;
+using SW_APIS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace SW_APIS.Services
 {
     public class BatchService
     {
-        
+        public static async void SendMail(List<string> emailTo, string subject)
+        {
+            //try
+            //{
+            //    var mensaje = new MimeMessage();
+            //    mensaje.From.Add(new MailboxAddress("Batch Service", ""));
+            //    mensaje.To.Add(new MailboxAddress("", to));
+            //    mensaje.Subject = subject;
+
+            //    var bodyBuilder = new BodyBuilder();
+            //    bodyBuilder.HtmlBody = content;
+            //    bodyBuilder.Attachments.Add(file);
+            //    mensaje.Body = bodyBuilder.ToMessageBody();
+
+            //    using (var client = new SmtpClient("myHost", myPort))
+            //    {
+            //        client.Send(mensaje);
+            //    }
+
+            //    estado = true;
+            //    return estado;
+            //}
+            //catch (Exception ex)
+            //{
+            //    return estado;
+            //}
+        }
+        public static ResponseModel ValidateRequest(BatchModel batchRequest)
+        {
+            if(batchRequest is null)
+            {
+                return new ResponseModel()
+                {
+                    status = "error",
+                    message = "El request viene vacío."
+                };
+            }
+            return new ResponseModel()
+            {
+                status = "success"
+            };
+        }
     }
 }
